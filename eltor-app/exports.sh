ece1931ffa68d6f8b95464d5a5578d8a8a0615a1
eltor <eltordev@proton.me>
2025-06-28 23:07:29 -0500

    cln exports

--- eltor-app/exports.sh
+++ eltor-app/exports.sh
@@ -1,43 +1,55 @@
-export APP_ELTOR_PORT="5173"
-export APP_HOST="eltor-app_web_1"
-export PHOENIXD_PASSWORD="test"
+######################################
+### App env vars
+######################################
+export APP_ELTOR_FRONTEND_PORT="5173"
+export APP_ELTOR_BACKEND_PORT="5174"
+export APP_ELTOR_HOST="eltor-app_web_1"
+export APP_ELTOR_USER_DIR="/home/user"
+export APP_ELTOR_ELTORRC_PATH="$APP_ELTOR_USER_DIR/code/eltor-app/backend/bin/data"
 
-##################################
-### Client Environment Variables
-##################################
-export TOR_ADDITIONAL_DIR_AUTHORITY=""
-# TODO user should be able to set this
-export TOR_NICKNAME="elfdbe78324"
-export TOR_DATA_DIRECTORY="/home/user/data/tor"
-export TOR_SOCKS_PORT="0.0.0.0:18058"
-export TOR_CONTROL_PORT="9992"
-# password1234_
-export TOR_HASHED_CONTROL_PASSWORD="16:281EC5644A4F548A60D50A0DD4DF835FFD50EDED062FD270D7269943DA"
-export TOR_ADDRESS="127.0.0.1"
-export TOR_PAYMENT_CIRCUIT_MAX_FEE="11000"
-# Placeholder - will be overridden by docker-compose environment
-export TOR_CLIENT_PAYMENT_LIGHTNING_NODE_CONFIG="placeholder"
+######################################
+### LN env vars
+######################################
+export APP_ELTOR_USE_PHOENIXD_EMBEDDED=false
+export APP_ELTOR_LN_IMPLEMENTATION=APP_ELTOR_TOR_CLIENT_PAYMENT_LIGHTNING_NODE_CONFIG
+export APP_ELTOR_LN_CONFIG=$APP_ELTOR_LN_CONFIG
 
-#################################
-### Relay Environment Variables
-#################################
-export TOR_RELAY_ADDITIONAL_DIR_AUTHORITY="" 
-export TOR_RELAY_DATA_DIRECTORY="/home/user/data/tor-relay"
-# export TOR_RELAY_ADDRESS="X.X.X.X" # public IP or domain of the relay
-export TOR_RELAY_CONTACT="eltorcontact"
+######################################
+### Client Eltor Environment Variables
+######################################
+export APP_ELTOR_TOR_ADDITIONAL_DIR_AUTHORITY=""
 # TODO user should be able to set this
-export TOR_RELAY_NICKNAME="elr123r5"
-export TOR_RELAY_OR_PORT="9996"
-export TOR_RELAY_CONTROL_PORT="7781"
-export TOR_RELAY_SOCKS_PORT="18057"
+export APP_ELTOR_TOR_NICKNAME="elfdbe78324"
+export APP_ELTOR_TOR_DATA_DIRECTORY="/home/user/data/tor"
+export APP_ELTOR_TOR_SOCKS_PORT="0.0.0.0:18058"
+export APP_ELTOR_TOR_CONTROL_PORT="9992"
 # password1234_
-export TOR_RELAY_HASHED_CONTROL_PASSWORD="16:281EC5644A4F548A60D50A0DD4DF835FFD50EDED062FD270D7269943DA"
-export TOR_RELAY_SANDBOX="0"
-export TOR_EXIT_RELAY="1"
-export TOR_RELAY_PAYMENT_BOLT12_OFFER="lno1234"
-export TOR_RELAY_PAYMENT_RATE_MSATS="1000"
-export TOR_RELAY_PAYMENT_INTERVAL="60"
-export TOR_RELAY_PAYMENT_INTERVAL_ROUNDS="10"
-export TOR_RELAY_PAYMENT_CIRCUIT_MAX_FEE="11000"
+export APP_ELTOR_TOR_HASHED_CONTROL_PASSWORD="16:281EC5644A4F548A60D50A0DD4DF835FFD50EDED062FD270D7269943DA"
+export APP_ELTOR_TOR_ADDRESS="127.0.0.1"
+export APP_ELTOR_TOR_PAYMENT_CIRCUIT_MAX_FEE="11000"
 # Placeholder - will be overridden by docker-compose environment
-export TOR_RELAY_PAYMENT_LIGHTNING_NODE_CONFIG="placeholder"
+export APP_ELTOR_TOR_CLIENT_PAYMENT_LIGHTNING_NODE_CONFIG="type=cln url=https://$APP_CORE_LIGHTNING_DAEMON_IP:$CORE_LIGHTNING_REST_PORT rune=$LIGHTNING_RUNE default=true"
+
+######################################
+### Relay Eltor Environment Variables
+######################################
+export APP_ELTOR_TOR_RELAY_ADDITIONAL_DIR_AUTHORITY="" 
+export APP_ELTOR_TOR_RELAY_DATA_DIRECTORY="/home/user/data/tor-relay"
+# export APP_ELTOR_TOR_RELAY_ADDRESS="X.X.X.X" # public IP or domain of the relay
+export APP_ELTOR_TOR_RELAY_CONTACT="eltorcontact"
+# TODO user should be able to set this
+export APP_ELTOR_TOR_RELAY_NICKNAME="elr123r5"
+export APP_ELTOR_TOR_RELAY_OR_PORT="9996"
+export APP_ELTOR_TOR_RELAY_CONTROL_PORT="7781"
+export APP_ELTOR_TOR_RELAY_SOCKS_PORT="18057"
+# password1234_
+export APP_ELTOR_TOR_RELAY_HASHED_CONTROL_PASSWORD="16:281EC5644A4F548A60D50A0DD4DF835FFD50EDED062FD270D7269943DA"
+export APP_ELTOR_TOR_RELAY_SANDBOX="0"
+export APP_ELTOR_TOR_EXIT_RELAY="1"
+export APP_ELTOR_TOR_RELAY_PAYMENT_BOLT12_OFFER="lno1234"
+export APP_ELTOR_TOR_RELAY_PAYMENT_RATE_MSATS="1000"
+export APP_ELTOR_TOR_RELAY_PAYMENT_INTERVAL="60"
+export APP_ELTOR_TOR_RELAY_PAYMENT_INTERVAL_ROUNDS="10"
+export APP_ELTOR_TOR_RELAY_PAYMENT_CIRCUIT_MAX_FEE="11000"
+# Placeholder - will be overridden by docker-compose environment
+export APP_ELTOR_TOR_RELAY_PAYMENT_LIGHTNING_NODE_CONFIG="type=cln url=https://$APP_CORE_LIGHTNING_DAEMON_IP:$CORE_LIGHTNING_REST_PORT rune=$LIGHTNING_RUNE default=true"
