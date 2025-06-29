4b66fe72d8d39a7ae86a4c0a0eb6f5d2afde48cc
eltor <eltordev@proton.me>
2025-06-29 00:32:41 -0500

    .

--- eltor-app/exports.sh
+++ eltor-app/exports.sh
@@ -5,7 +5,8 @@ export APP_ELTOR_FRONTEND_PORT="5173"
 export APP_ELTOR_BACKEND_PORT="5174"
 export APP_ELTOR_HOST="eltor-app_web_1"
 export APP_ELTOR_USER_DIR="/home/user"
-export APP_ELTOR_ELTORRC_PATH="$APP_ELTOR_USER_DIR/backend/bin/data"
+# TODO move to /home/user/.eltor aka ~/.eltor
+export APP_ELTOR_ELTORRC_PATH="/home/user/code/eltor-app/backend/bin/data"
 
 ######################################
 ### LN env vars
@@ -20,7 +21,7 @@ export APP_ELTOR_LN_BOLT12="lno1234"
 export APP_ELTOR_TOR_ADDITIONAL_DIR_AUTHORITY=""
 # TODO user should be able to set this
 export APP_ELTOR_TOR_NICKNAME="elfdbe78324"
-export APP_ELTOR_TOR_DATA_DIRECTORY="/home/user/data/tor"
+export APP_ELTOR_TOR_DATA_DIRECTORY="/home/user/.tor"
 export APP_ELTOR_TOR_SOCKS_PORT="0.0.0.0:18058"
 export APP_ELTOR_TOR_CONTROL_PORT="9992"
 # password1234_
@@ -32,7 +33,7 @@ export APP_ELTOR_TOR_PAYMENT_CIRCUIT_MAX_FEE="11000"
 ### Relay Eltor Environment Variables
 ######################################
 export APP_ELTOR_TOR_RELAY_ADDITIONAL_DIR_AUTHORITY="" 
-export APP_ELTOR_TOR_RELAY_DATA_DIRECTORY="/home/user/data/tor-relay"
+export APP_ELTOR_TOR_RELAY_DATA_DIRECTORY="/home/user/.tor/relay"
 # export APP_ELTOR_TOR_RELAY_ADDRESS="X.X.X.X" # public IP or domain of the relay
 export APP_ELTOR_TOR_RELAY_CONTACT="eltorcontact"
 # TODO user should be able to set this
